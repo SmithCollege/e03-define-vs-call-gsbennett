5e8432ab812faee0d88cfb973972c04a89dc5d61
gsbennett <gsbennett@smith.edu>
2025-02-12 15:36:20 +0000

    in class parctice

--- e03_main.py
+++ e03_main.py
@@ -13,3 +13,15 @@ def function3 (num):
 
 # TODO:
 # we can call these functions too!. Do so under this line
+res1= function1() #you arent catching anything because theres no return.
+print(res1) #printing none.
+res2 = function2()
+print(res2)
+res3 = function3( res2 )
+print(res3)
+
+def timesfour(num4):
+  return num4*4
+x = 3
+res4 = timesfour(x)
+print(res4)
